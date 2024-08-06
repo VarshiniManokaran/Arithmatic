82f667f73077384eb178ad23b7ca654c54338c44
VarshiniManokaran <varshini6678@gmail.com>
2024-08-06 15:27:46 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,20 @@
+
+
+public class Arithmatic {
+    public static void main(String[] args) {
+    //Arithmatic operators:+,-,*,/;
+    
+    int a=2345;
+    int b=4564;
+    int d=8765;
+    int e=6;
+    int f=56;
+    int c=a+b-d*e/f;
+    System.out.println(b*e);
+    System.out.println(c);
+    System.out.println(f/b);
+
+
+    
+}
+}
